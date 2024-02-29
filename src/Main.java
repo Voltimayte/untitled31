138308a64ecf850d37c42c79820ad151f08f0d60
Voltimayte <foxzam3@gmail.com>
2024-02-29 16:53:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner soot = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = soot
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
